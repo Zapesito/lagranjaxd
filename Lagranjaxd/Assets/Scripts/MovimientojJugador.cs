49c3fbbfae180068c6640c7683c0c9218066922b
unknown <pipepele@hotmail.com>
2025-08-04 12:00:09 -0600

    v9

---
+++ Lagranjaxd/Assets/Scripts/MovimientojJugador.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovimientojJugador : MonoBehaviour
+{
+    public float sprint = 5f;
+    public Rigidbody2D rg;
+    public Vector2 entrada;
+    public GameObject preFabTrigo;
+    public GameObject preFabTomate;
+    Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rg.linearVelocity = entrada * sprint;
+    }
+    public void Moverse(InputAction.CallbackContext contexto)
+    {
+        animator.SetBool("estaCaminando", true);
+        entrada = contexto.ReadValue<Vector2>();
+
+        Vector2 valorEntrada = contexto.ReadValue<Vector2>();
+
+        // Determinar el eje dominante
+        if (Mathf.Abs(valorEntrada.x) > Mathf.Abs(valorEntrada.y))
+        {
+            // Movimiento horizontal
+            entrada = new Vector2(Mathf.Sign(valorEntrada.x), 0);
+        }
+        else if (Mathf.Abs(valorEntrada.y) > 0)
+        {
+            // Movimiento vertical
+            entrada = new Vector2(0, Mathf.Sign(valorEntrada.y));
+        }
+        else
+        {
+            entrada = Vector2.zero;
+        }
+
+        animator.SetFloat("entradaX", entrada.x);
+        animator.SetFloat("entradaY", entrada.y);
+
+        if(contexto.canceled){
+           animator.SetBool("estaCaminando", false); 
+    }
+
+    }
+    public void sembrarT(InputAction.CallbackContext contexto)
+    {
+        if (contexto.started)
+        {
+            Instantiate(preFabTrigo, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void sembrarJ(InputAction.CallbackContext contexto)
+    {
+        if (contexto.started)
+        {
+            Instantiate(preFabTomate, transform.position, Quaternion.identity);
+        }
+    }
+
+        private void OntriggerEnter2D(Collider2D colision){
+            if (colision.CompareTag("huevo")){
+                Destroy(colision.gameObject);
+                GameManager.instancia.SumarHuevo();
+            }
+            }
+
+
+
+
+
+}
+
+
+
